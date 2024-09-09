b554596918b2439e87af0ba3842f472660cd675f
Zach Palmer <apex1421@outlook.com>
2024-09-09 08:55:39 -0500

    init4

--- cmd/portfolio/main.go
+++ cmd/portfolio/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "net/http"
+
 func main() {
 
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir(config.Static))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("GET /", index)
+	mux.HandleFunc("/err", err)
+
+	mux.HandleFunc("GET /resume", myResume)
+	mux.HandleFunc("GET /zypher", zypherToolsGet)
+	mux.HandleFunc("POST /zypher", zypherToolsPost)
+	mux.HandleFunc("GET /shiftapproval", shiftApprovalManagerGet)
+	mux.HandleFunc("POST /shiftapproval", shiftApprovalManagerPost)
+	mux.HandleFunc("GET /schedule", myScheduleGet)
+	mux.HandleFunc("POST /schedule", mySchedulePost)
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }
---
+++ internal/models/Details.go
@@ -0,0 +1,54 @@
+package models
+
+type XpItem struct {
+	Item string
+	Yrs  float32
+}
+
+type PrevWork struct {
+	Company     string
+	Duration    float32
+	Description string
+}
+
+type Details struct {
+	Name        string
+	Title       string
+	Languages   map[string][]XpItem
+	Exp         []PrevWork //
+	ExpOverview string     // description of experience
+	Misc        string
+}
+
+func NewDetails(nm, ttl, xpOvw, msc string, exp []PrevWork, langs map[string][]XpItem) *Details {
+	return &Details{
+		Name:        nm,
+		Title:       ttl,
+		Exp:         exp,
+		ExpOverview: xpOvw,
+		Misc:        msc,
+		Languages:   langs,
+	}
+}
+
+func NewXpItem(item string, yrs float32) *XpItem {
+	return &XpItem{
+		Item: item,
+		Yrs:  yrs,
+	}
+}
+
+func NewPrevWork(company, descript string, duration float32) *PrevWork {
+	return &PrevWork{
+		Company:     company,
+		Description: descript,
+		Duration:    duration,
+	}
+}
+
+// <li>Go</li>
+// <li>Python</li>
+// <li>Java</li>
+// <li>SQL</li>
+// <li>Postgres</li>
+// <li>MongoDB</li>
